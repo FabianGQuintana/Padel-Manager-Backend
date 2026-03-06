e65c507210b51b7d943e0884685319948d0ddf0d
Fabian Quintana <fabianquintana980@gmail.com>
2026-03-05 23:07:57 -0300

    feat: add initial domain entities and infrastructure dependencies

---
+++ PadelManager.Domain/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PadelManager.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public required string Name { get; set; } 
+
+        public  string? Description { get; set; }
+
+        //Relationships FK
+        public Guid TournamentId { get; set; }
+
+        //Navigation properties
+        public required Tournament Tournament { get; set; }
+    }
+}
---
+++ PadelManager.Domain/Entities/Tournament.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PadelManager.Domain.Entities
+{
+    public class Tournament : BaseEntity
+    {
+        public required string Name { get; set; }
+
+        public required DateTime StartDate { get; set; }
+
+        public required string Regulations { get; set; }
+
+        //Relationships FK
+        public Guid ManagerId { get; set; }
+
+        //Navigation properties
+        public required Manager Manager { get; set; }
+
+    }
+}
