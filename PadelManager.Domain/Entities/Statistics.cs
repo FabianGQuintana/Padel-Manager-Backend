3159e2c56a85ef2b128253e05fd66aa864b0c386
Ivan-Gauto <ivangauto602160@gmail.com>
2026-03-07 15:41:19 -0300

    feat: add statistics entity

---
+++ PadelManager.Domain/Entities/Statistics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PadelManager.Domain.Entities
+{
+    internal class Statistics
+    {
+        public int IdStatistics { get; set; }
+        public int Wo { get; set; }
+        public int SetsWon { get; set; }
+        public int SetsLost { get; set; }
+        public int GamesWon { get; set; }
+        public int GamesLost { get; set; }
+
+        public int IdCouple { get; set; }
+        public int IdZone { get; set; }
+
+        public virtual Couple Couple { get; set; } = null!;
+        public virtual Zone Zone { get; set; } = null!;
+    }
+}
