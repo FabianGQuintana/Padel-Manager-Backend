ff7e5881a609356af66f0d98c5b27e0aa437bc1d
Ivan-Gauto <ivangauto602160@gmail.com>
2026-03-08 18:43:15 -0300

    feat: Refactor Statistics class to inherit from BaseEntity

--- PadelManager.Domain/Entities/Statistics.cs
+++ PadelManager.Domain/Entities/Statistics.cs
@@ -4,18 +4,26 @@ using System.Text;
 
 namespace PadelManager.Domain.Entities
 {
-    internal class Statistics
+    public class Statistics : BaseEntity
     {
-        public int IdStatistics { get; set; }
         public int Wo { get; set; }
+
         public int SetsWon { get; set; }
+
         public int SetsLost { get; set; }
+
         public int GamesWon { get; set; }
+
         public int GamesLost { get; set; }
 
-        public int IdCouple { get; set; }
-        public int IdZone { get; set; }
-        public virtual Couple Couple { get; set; } = null!;
-        public virtual Zone Zone { get; set; } = null!;
+        //Relationships FK
+        public Guid CoupleId { get; set; }
+
+        public Guid ZoneId { get; set; }
+
+        //Navigation properties
+        public required Couple Couple { get; set; }
+
+        public required Zone Zone { get; set; }
     }
 }
