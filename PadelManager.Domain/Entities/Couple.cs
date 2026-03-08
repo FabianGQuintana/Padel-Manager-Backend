b80dea64b3e9ed1fe664136b7858ddb7bf331d75
Maximo-21 <maxiximo21@gmail.com>
2026-03-08 16:03:22 -0300

    entidades

---
+++ PadelManager.Domain/Entities/Couple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PadelManager.Domain.Entities
+{
+    public class Couple : BaseEntity
+    {
+        public required string Category { get; set; } // Categoría a la que pertenece la pareja (Veteranos, Libres, Menores, etc.)
+        public string? Nickname { get; set; } // Apodo o nombre de la pareja (opcional)
+        // Relationships FK
+        public Guid PlayerId { get; set; }
+        // Navigation properties
+        public required Player Player { get; set; }
+        // Relationships FK
+        public Guid ZonaId { get; set; }
+        // Navigation properties
+        public required Zone Zone { get; set; }
+    }
+}
---
+++ PadelManager.Domain/Entities/Stage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PadelManager.Domain.Entities
+{
+    public class Stage : BaseEntity
+    {
+        public required string Group { get; set; } // Grupo A, B, C, etc.
+        public required string Deletion { get; set; }
+        public required string Order { get; set; } // Para ordenar las fases (1, 2, 3, etc.)
+
+        // Relationships FK
+        public Guid CategoryId { get; set; }
+
+        // Navigation properties
+        public required Category Category { get; set; }
+
+    }
+}
