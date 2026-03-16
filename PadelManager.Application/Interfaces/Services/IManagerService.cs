c8daa9ec3210df61d4c5133cd9c7e539433408b3
Fabian Quintana <fabianquintana980@gmail.com>
2026-03-16 17:51:42 -0300

    feat: Added folders for Dto's, Stage DB finally

---
+++ PadelManager.Application/Interfaces/Services/IManagerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PadelManager.Application.Interfaces.Services
+{
+    public class IManagerService
+    {
+        
+    }
+}
