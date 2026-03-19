d5d21655c4a9341291b0f5f5e9822f5ed53f7161
Ivan-Gauto <ivangauto602160@gmail.com>
2026-03-19 18:11:56 -0300

    feat: Dtos for Statistic service have been added

---
+++ PadelManager.Application/DTOs/Statistic/CreateStatisticDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PadelManager.Application.DTOs.Statistic
+{
+    public class CreateStatisticDto
+    {
+        [Required] 
+        public required Guid CoupleId { get; set; }
+
+        [Required]
+        public required Guid ZoneId { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Points { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int WoCount { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int MatchesPlayed { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int MatchesWon { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int SetsWon { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int SetsLost { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int GamesWon { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int GamesLost { get; set; }
+    }
+}
