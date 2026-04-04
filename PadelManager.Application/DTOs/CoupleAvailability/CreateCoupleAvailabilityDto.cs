bcc4fc594ce05d1b2181edd8d59b59333b9f1d13
Maximo-21 <maxiximo21@gmail.com>
2026-04-04 03:56:59 -0300

    Update CreateCoupleAvailabilityDto: change required field to nullable

--- PadelManager.Application/DTOs/CoupleAvailability/CreateCoupleAvailabilityDto.cs
+++ PadelManager.Application/DTOs/CoupleAvailability/CreateCoupleAvailabilityDto.cs
@@ -9,7 +9,7 @@ namespace PadelManager.Application.DTOs.CoupleAvailability
         public required DayOfWeek Day { get; set; } // Día de la semana (Lunes, Martes, etc.)
         public required TimeOnly From { get; set; } // Hora de inicio de la disponibilidad
         public required TimeOnly To { get; set; } // Hora de fin de la disponibilidad
-        public required Guid CoupleId { get; set; }
+        public Guid? CoupleId { get; set; }
 
     }
 }
